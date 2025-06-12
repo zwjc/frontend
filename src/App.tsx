73dac0afbe736c1a7e82cc97d959f513e1372c0a
Jeffrey Chang <jeffreyc@kth.se>
2025-06-12 17:02:34 +0200

    feat: simple hello world ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '2rem' }}>
+    <h1>🚀 Hello, Cloudflare Pages!</h1>
+    <p>Your React + TypeScript site is live.</p>
+  </div>
+);
 
 export default App;
+
