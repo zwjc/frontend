696edab30c2c84d4668aedfcb067c12cf59d4925
Jeffrey Chang <jeffreyc@kth.se>
2025-06-24 20:35:11 +0200

    fix: revert commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './home';
-import Products from './products';
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-    <Route path="/about" element={<div>About Us Page</div>} />
-    <Route path="/contact" element={<div>Contact Page</div>} />
     {/* Add more routes here */}
   </Routes>
 );
